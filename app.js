5a8633d7410b82deec1b37cf3dda95681cb93361
Hernan_Vasquez <nachjvasquez@gmail.com>
2026-04-13 10:10:07 -0500

    conexion de api usando fetch

---
+++ app.js
@@ -0,0 +1,31 @@
+// //hacemos conexion de items con la PokeAPI
+// function pruebaAPI(){
+// fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => console.error(error));
+// }
+// //probamos salida en consola
+// console.log(pruebaAPI);
+// //probamos ver solo por nombre
+// function pruebaAPInombre(){
+// fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+//   .then(res => res.json())
+//   .then(data => {
+//     data.results.forEach(pokemon => {
+//         console.log(pokemon.name);
+//     });
+//   });
+// }
+// console.log(pruebaAPInombre);
+function llamarPokeAPI(){
+const lista = document.getElementById("pokemon-list");
+fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => console.error(error));
+}
